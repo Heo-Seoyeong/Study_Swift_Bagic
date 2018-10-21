614777f9a6fc8d13976d403f51ca7fbbec1702d7
Seoyeong <gjtjdud93@gmail.com>
2018-10-21 22:16:39 +0900

    10. 반복문 11. 옵셔널 12. 옵셔널 값 추출

---
+++ Lecture/4회/MyPlayground.playground/Contents.swift
@@ -0,0 +1,137 @@
+/* 반복문 */
+
+var integers = [1, 2, 3]
+let people = ["seoyeong": 10, "dung": 15, "heo": 12]
+
+//MARK: - for-in
+
+//for <#item#> in <#items#> {
+//    <#code#>
+//}
+
+for integer in integers {
+    print(integer)
+}
+
+// Dictionary의 item은 key와 value로 구성된 튜플 타입입니다
+for (name, age) in people {
+    print("\(name): \(age)")
+}
+
+//MARK: - while
+
+//while <#condition#> {
+//    <#code#>
+//}
+
+while integers.count > 1 {
+    integers.removeLast()
+}
+
+//MARK: - repeat-while
+
+repeat {
+    integers.removeLast()
+} while integers.count > 0
+
+
+
+/* 옵셔널 */
+
+//MARK:- 암시적 추출 옵셔널
+// Implicitly Unwrapped Optional
+var implicitlyUnwrappedOptionalValue: Int! = 100
+
+switch implicitlyUnwrappedOptionalValue {
+case .none:
+    print("This Optional variable is nil")
+case .some(let value):
+    print("Value is \(value)")
+}
+
+// 기존 변수처럼 사용 가능
+implicitlyUnwrappedOptionalValue = implicitlyUnwrappedOptionalValue + 1
+
+// nil 할당 가능
+implicitlyUnwrappedOptionalValue = nil
+
+// 잘못된 접근으로 인한 런타임 오류 발생
+//implicitlyUnwrappedOptionalValue = implicitlyUnwrappedOptionalValue + 1
+
+//MARK:- 옵셔널
+// Optional
+var optionalValue: Int? = 100
+
+switch optionalValue {
+case .none:
+    print("This Optional variable is nil")
+case .some(let value):
+    print("Value is \(value)")
+}
+
+// nil 할당 가능
+optionalValue = nil
+
+// 기존 변수처럼 사용불가 - 옵셔널과 일반 값은 다른 타입이므로 연산불가
+//optionalValue = optionalValue + 1
+
+
+
+/* 옵셔널 추출 */
+
+// 옵셔널 바인딩
+// 강제 추출
+//MARK:- Optional Binding
+func printName(_ name: String) {
+    print(name)
+}
+
+var myName: String? = nil
+
+//printName(myName)
+// 전달되는 값의 타입이 다르기 때문에 컴파일 오류발생
+if let name: String = myName {
+    printName(name)
+} else {
+    print("myName == nil")
+}
+
+
+var yourName: String! = nil
+
+if let name: String = yourName {
+    printName(name)
+} else {
+    print("yourName == nil")
+}
+
+// name 상수는 if-let 구문 내에서만 사용가능합니다
+// 상수 사용범위를 벗어났기 때문에 컴파일 오류 발생
+//printName(name)
+// ,를 사용해 한 번에 여러 옵셔널을 바인딩 할 수 있습니다
+// 모든 옵셔널에 값이 있을 때만 동작합니다
+myName = "seoyeong"
+yourName = nil
+
+if let name = myName, let friend = yourName {
+    print("\(name) and \(friend)")
+}
+// yourName이 nil이기 때문에 실행되지 않습니다
+yourName = "dung"
+
+if let name = myName, let friend = yourName {
+    print("\(name) and \(friend)")
+}
+// yagom and hana
+
+//MARK:- Force Unwrapping
+printName(myName!) // yagom
+myName = nil
+
+//print(myName!)
+// 강제추출시 값이 없으므로 런타임 오류 발생
+yourName = nil
+
+//printName(yourName)
+// nil 값이 전달되기 때문에 런타임 오류발생
+
